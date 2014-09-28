0af3a15ef49bbf10386721ef97f76940357ecf46
Sunjin Lee <styner32@gmail.com>
2014-09-28 17:25:22 +0800

    rendering waveform (WIP)

--- app/assets/javascripts/editor.js.jsx
+++ app/assets/javascripts/editor.js.jsx
@@ -1,6 +1,7 @@
 /** @jsx React.DOM */
 
 // http://content.viki.com/44699v/44699v_240p_1312010824.mp4
+// http://x1.vikiassets.com/waveforms/c90ae0568f91df5583ff7fd8bf460512c5c212bb.json
 
 var VideoUrlForm = React.createClass({
   handleSubmit: function(e) {
@@ -18,8 +19,45 @@ var VideoUrlForm = React.createClass({
     return (
       <form className="videoUrlForm" onSubmit={this.handleSubmit}>
         <input type="text" placeholder="Video Url" ref="videoUrl"/>
-        <input type="submit" value="Load" />
+        <input type="submit" value="Load Video" />
       </form>
+    );
+  }
+});
+
+var WaveformUrlForm = React.createClass({
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var waveformUrl = this.refs.waveformUrl.getDOMNode().value.trim();
+    if (!waveformUrl) {
+      return;
+    }
+    this.props.onWaveformUrlSubmit({waveformUrl: waveformUrl});
+    this.refs.waveformUrl.getDOMNode().value = '';
+    return;
+  },
+
+  render: function() {
+    return (
+      <form className="waveformUrlForm" onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Waveform Url" ref="waveformUrl"/>
+        <input type="submit" value="Load Waveform" />
+      </form>
+    );
+  }
+});
+
+var Waveform = React.createClass({
+  render: function() {
+    new Waveform({
+        container: this.refs.waveform.getDOMNode(),
+        innerColor: "blue",
+        outerColor: "gray",
+        data: data,
+        width: data.length,
+        height: 50});
+    return (
+      <div className="waveform" ref='waveform'></div>
     );
   }
 });
@@ -30,12 +68,32 @@ var App = React.createClass({
     return;
   },
 
+  loadWaveform: function(params) {
+    var url = params.waveformUrl;
+    $.ajax({
+      url: url,
+      type: 'GET',
+      async: false,
+      jsonpCallback: "parseResponse",
+      contentType: "application/json",
+      dataType: 'jsonp'
+    }).done(function( data ) {
+
+    });
+    return;
+  },
+
+  getInitialState: function() {
+    return {waveformData: []};
+  },
+
   render: function() {
     return (
       <div className='app'>
         <VideoUrlForm onVideoUrlSubmit={this.loadVideo}/>
-        <video className="player" width="320" height="240" ref='video' controls>
-        </video>
+        <WaveformUrlForm onWaveformUrlSubmit={this.loadWaveform}/>
+        <video className="player" width="320" height="240" ref='video' controls></video>
+        <Waveform data={this.state.waveformData} />
       </div>
     )
   }
